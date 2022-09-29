c795e83d1eb2d27a3c501a89c491c4af4955b674
Александр Бахмутов <alexdmitralex@gmail.com>
2022-09-29 18:29:18 +0300

    Создал главный объект App и инициализировал его в Main

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class App extends JFrame {
+    Container container;
+
+    public App() throws HeadlessException {
+        super("Лабораторная номер 1");
+
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        App fileSelectExample = new App();
+        fileSelectExample.setVisible(true);
+    }
+}
