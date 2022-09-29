fb75371bd67691d1c1cfe3adbe0827aad574f3c5
Александр Бахмутов <alexdmitralex@gmail.com>
2022-09-29 18:56:50 +0300

    Добавил расположение кнопок

---
+++ src/main/java/org/example/FramePrinter.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FramePrinter {
+
+    public static void printDownloudWindow(JFrame frame, String fileStatus) {
+        frame.setSize(300, 200);
+        frame.setResizable(false);
+        frame.getRootPane().setBorder
+                (BorderFactory.createEmptyBorder
+                        (20, 10, 20, 10));
+
+        // Панель содержимого
+        Container container = frame.getContentPane();
+        // Устанавливаем менеджер последовательного расположения
+        container.setLayout(new GridLayout
+                (2, 0, 10, 50));
+
+        JLabel jl = new JLabel("Выбирите файл");
+        JButton buttonOpen = ButtomFactory.createButtonOpen();
+        JLabel status = new JLabel(fileStatus);
+
+        container.add(jl);
+        container.add(buttonOpen);
+        container.add(status);
+
+        frame.revalidate();
+        frame.repaint();
+    }
+
+
+}
