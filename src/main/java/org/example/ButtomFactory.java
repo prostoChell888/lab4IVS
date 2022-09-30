74ee29377d027fc87dd394559b62c8ddd376fe9e
Александр Бахмутов <alexdmitralex@gmail.com>
2022-09-30 08:58:19 +0300

    Рабочая программа !

--- src/main/java/org/example/ButtomFactory.java
+++ src/main/java/org/example/ButtomFactory.java
@@ -1,11 +1,28 @@
 package org.example;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
+import java.awt.*;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.List;
 
+import static java.awt.Component.LEFT_ALIGNMENT;
+
 public class ButtomFactory {
+
+    public static JButton createTable(JFrame frame, List<Location> listWithCoordinates) {
+        JButton jButton = new JButton("Таблица");
+        jButton.addActionListener(x -> {
+
+            try {
+                FramePrinter.printTableWindow(frame, listWithCoordinates);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        return jButton;
+    }
 
 
     public static JButton createButtonOpen(JFrame frame) {
@@ -24,17 +41,95 @@ public class ButtomFactory {
                                 CordParser.parseTxtFile(chosenFile);
                         FramePrinter.printTableWindow(frame, listWithCoordinates);
 
-                    }catch (Exception e) {
+                    } catch (Exception e) {
                         FramePrinter.printDownloudWindow(frame, e.getMessage());
                     }
-
-
                 }
             }
         });
 
-       return  jButton;
+        return jButton;
+    }
+
+    public static JButton createLoadFileButton(JFrame frame) {
+        JButton jButton = new JButton("Выбрать файл");
+        jButton.addActionListener(x -> {
+            FramePrinter.printDownloudWindow(frame, "Файл н евыбран");
+        });
+        return jButton;
     }
 
 
+    public static JComboBox createComboBoxChooseAxis
+            (List<Double> cords, List<Location> locationList, StringBuilder nameOfAxis) {
+        Font font = new Font("Verdana", Font.PLAIN, 18);
+
+        String[] items = {
+                "Направление",
+                "Время",
+                "Широта",
+                "Долгота",
+                "Высота",
+                "скорость"
+        };
+
+        ActionListener actionListener = e -> {
+            JComboBox box = (JComboBox) e.getSource();
+            String item = (String) box.getSelectedItem();
+            nameOfAxis.setLength(0);
+            nameOfAxis.append(item);
+            switch (item) {
+                case "Время":
+
+                    for (var cord : locationList)
+                        cords.add(cord.getTimeInHours());
+                    break;
+                case "Широта":
+                    for (var cord : locationList)
+                        cords.add(cord.getLatitudeInDegrees());
+                    break;
+                case "Долгота":
+                    for (var cord : locationList)
+                        cords.add(cord.getLongitudeInDegrees());
+                    break;
+                case "Высота":
+                    for (var cord : locationList)
+                        cords.add(cord.getAltitudeInDegrees());
+                    break;
+                case "Направление":
+                    for (var cord : locationList)
+                        cords.add(cord.getCourse());
+                    break;
+                case "скорость":
+                    for (var cord : locationList)
+                        cords.add(cord.getSpeedInKilPerHour());
+                    break;
+            }
+        };
+
+        JComboBox comboBox = new JComboBox(items);
+        comboBox.setFont(font);
+        comboBox.setAlignmentX(LEFT_ALIGNMENT);
+        comboBox.addActionListener(actionListener);
+
+        return comboBox;
+    }
+
+    public static JButton createGraphButton
+            (JFrame frame, List<Double> listXAxis,
+             List<Double> listYAxis, StringBuilder nameOfXAxis,
+             StringBuilder nameOfYAxis, List<Location> listWithCoordinates) {
+
+        JButton jButton = new JButton("Построить график");
+
+        ActionListener actionListener = e -> {
+            FramePrinter.printGraphWindow(frame, listXAxis,
+                    listYAxis, nameOfXAxis, nameOfYAxis, listWithCoordinates);
+
+        };
+        jButton.addActionListener(actionListener);
+
+
+        return jButton;
+    }
 }
--- src/main/java/org/example/FramePrinter.java
+++ src/main/java/org/example/FramePrinter.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FramePrinter {
@@ -35,10 +44,10 @@ public class FramePrinter {
 
     public static void printTableWindow(JFrame frame, List<Location> listWithCoordinates) throws Exception {
 
-       if (listWithCoordinates == null){
+        if (listWithCoordinates == null) {
             throw new Exception("Невозможно считать содерижиое файла");
         }
-        Container container= frame.getContentPane();
+        Container container = frame.getContentPane();
         container.removeAll();
         container.setLayout(new BorderLayout());
 
@@ -48,12 +57,35 @@ public class FramePrinter {
         JTable table = getjTable(listWithCoordinates);
         JScrollPane scrollPane = new JScrollPane(table);
         container.add(scrollPane, BorderLayout.CENTER);
+        //container.add(scrollPane);
+
+        List<Double> listXAxis = new ArrayList<>();
+        List<Double> listYAxis = new ArrayList<>();
+
+        StringBuilder nameOfXAxis = new StringBuilder();
+        StringBuilder nameOfYAxis = new StringBuilder();
+        JComboBox boxAxisX = ButtomFactory.createComboBoxChooseAxis(listXAxis, listWithCoordinates, nameOfXAxis);
+        JComboBox boxAxisY = ButtomFactory.createComboBoxChooseAxis(listYAxis, listWithCoordinates, nameOfYAxis);
+
+        JButton loadFileButton = ButtomFactory.createLoadFileButton(frame);
+
+        JButton graphButton = ButtomFactory.createGraphButton(frame, listXAxis, listYAxis, nameOfXAxis, nameOfYAxis, listWithCoordinates);
 
 
-//        JPanel grid = new JPanel(new GridLayout(1, 2, 5, 0) );
-//        JPanel flow = new JPanel(new FlowLayout(FlowLayout.CENTER));
-//        flow.add(grid);
-//        flow.setPreferredSize(new Dimension(100,10));
+        //var boxLayout = ;
+        JPanel grid = new JPanel(new GridLayout(6, 1, 0, 5));
+
+        grid.add(loadFileButton);
+        grid.add(new Label("Ось X"));
+        grid.add(boxAxisX);
+        grid.add(new Label("Ось Y"));
+        grid.add(boxAxisY);
+        grid.add(graphButton);
+
+
+        container.add(grid, BorderLayout.WEST);
+
+//
 
 
         frame.revalidate();
@@ -63,9 +95,9 @@ public class FramePrinter {
 
     private static JTable getjTable(List<Location> listWithCoordinates) {
 
-        String[] columnNames = { "Идентификационный Код",
-                "Дата", "Время (часы)","Широта(градусы)","N/S","Долгота(градусы)",
-                "E/W","Скорость (км/ч)",
+        String[] columnNames = {"Идентификационный Код",
+                "Дата", "Время (часы)", "Широта(градусы)", "N/S", "Долгота(градусы)",
+                "E/W", "Скорость (км/ч)",
                 "Направление(градусы)", "Высота(градусы)",
         };
 
@@ -85,5 +117,55 @@ public class FramePrinter {
         }
 
         return new JTable(data, columnNames);
+    }
+
+    public static void printGraphWindow
+            (JFrame frame, List<Double> listXAxis,
+             List<Double> listYAxis, StringBuilder nameOfXAxis,
+             StringBuilder nameOfYAxis, List<Location> listWithCoordinates) {
+        Container container = frame.getContentPane();
+        container.removeAll();
+        container.setLayout(new BorderLayout());
+
+        XYDataset dataset = createDatasetForSpeedWithTime(listXAxis, listYAxis);
+
+        JFreeChart chart = ChartFactory.createScatterPlot(
+                "График зависимости",
+                nameOfXAxis.toString(),//x
+                nameOfYAxis.toString(),//y
+                dataset);
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(new Color(196, 217, 255));
+
+        ChartPanel panel = new ChartPanel(chart);
+        container.add(panel, BorderLayout.CENTER);
+
+        JPanel grid = new JPanel(new GridLayout(2, 1, 0, 5));
+
+        JButton loadFileButton = ButtomFactory.createLoadFileButton(frame);
+        JButton tableBitton = ButtomFactory.createTable(frame, listWithCoordinates);
+
+        grid.add(loadFileButton);
+        grid.add(tableBitton);
+        container.add(grid, BorderLayout.WEST);
+
+
+        frame.revalidate();
+        frame.repaint();
+
+    }
+
+    private static XYDataset createDatasetForSpeedWithTime
+            (List<Double> listXAxis, List<Double> listYAxis) {
+        XYSeriesCollection dataset = new XYSeriesCollection();
+
+        XYSeries series = new XYSeries("");
+
+        for (int i = 0; i < listXAxis.size(); i++) {
+            series.add(listXAxis.get(i), listYAxis.get(i));
+        }
+
+        dataset.addSeries(series);
+        return dataset;
     }
 }
