58cab994f7ceae10eb8268218193faf5ef45ce4a
Александр Бахмутов <alexdmitralex@gmail.com>
2022-10-19 15:51:53 +0300

    добавил класс для хранения данных положения объекта которы пересекаются у 2 форматов

---
+++ src/main/java/org/example/FormatClases/PosInform.java
@@ -0,0 +1,10 @@
+package org.example.FormatClases;
+
+public class PosInform {
+    Double TimeUTC;//распарсить в часы
+    Double latitude; //распрсить
+    Character indicatorNS;
+    Double longitude;
+    Character indicatorEW;
+
+}
