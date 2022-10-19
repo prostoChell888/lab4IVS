bf1e70627a3eaf3d24cbebe48db6eeb6b4b5113b
Александр Бахмутов <alexdmitralex@gmail.com>
2022-10-19 20:16:12 +0300

    добавил геттеры и сеторы в PosInform

--- src/main/java/org/example/FormatClases/PosInform.java
+++ src/main/java/org/example/FormatClases/PosInform.java
@@ -7,4 +7,43 @@ public class PosInform {
     Double longitude;
     Character indicatorEW;
 
+    public Double getTimeUTC() {
+        return TimeUTC;
+    }
+
+    public void setTimeUTC(Double timeUTC) {
+        TimeUTC = timeUTC;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Character getIndicatorNS() {
+        return indicatorNS;
+    }
+
+    public void setIndicatorNS(Character indicatorNS) {
+        this.indicatorNS = indicatorNS;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Character getIndicatorEW() {
+        return indicatorEW;
+    }
+
+    public void setIndicatorEW(Character indicatorEW) {
+        this.indicatorEW = indicatorEW;
+    }
 }
