46c9bdd06bfcdfcf1c415045674101dc48d22c7f
Александр Бахмутов <alexdmitralex@gmail.com>
2022-10-19 19:29:30 +0300

    првоел рефакторинг выделил методы из метода addInfoFromGafFile

--- src/main/java/org/example/DBConector.java
+++ src/main/java/org/example/DBConector.java
@@ -3,46 +3,20 @@ package org.example;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
+
 public class DBConector {
 
 
     public void addInfoFromRafFile(File file) throws Exception {
 
-        String bufString;
+
         try (InputStream is = new FileInputStream(file);
              InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
              BufferedReader br = new BufferedReader(isr)) {
+            var standarts = new Standarts();
+            while (getFormatsFromBuffer(br, standarts)) {
 
 
-
-
-            while ((bufString = br.readLine()) != null) {
-
-                boolean f = true;
-                String[] arrOfLocationDataInStrings;
-                do {
-                    int startOfStandartStr = bufString.indexOf("$GP");
-                    if (startOfStandartStr == -1) continue;
-
-                    arrOfLocationDataInStrings = bufString.substring(startOfStandartStr).split(",");
-                    switch (arrOfLocationDataInStrings[0]){
-                        case "$GPGGA":
-                            ParserCordFormats.GGA_Parser(arrOfLocationDataInStrings);
-                            break;
-                        case "$GPGSV":
-                            ParserCordFormats.GSV_Parser(arrOfLocationDataInStrings);
-                            break;
-                        case "$GPGSA":
-                            ParserCordFormats.GSA_Parser(arrOfLocationDataInStrings);
-                            break;
-                        case "$GPRMC":
-                            ParserCordFormats.RMC_Parser(arrOfLocationDataInStrings);
-                            f = false;
-                            break;
-                        default:
-                            //throw new Exception("Ошибка считывания файла");
-                    }
-                } while ((bufString = br.readLine()) != null && f);
             }
 
 
@@ -51,5 +25,38 @@ public class DBConector {
                     ex.getMessage());
         }
 
+    }
+
+    private static boolean getFormatsFromBuffer(BufferedReader br, Standarts standarts) throws Exception {
+        String bufString = null;
+        boolean f = true;
+        String[] arrOfLocationDataInStrings;
+
+        while (f && (bufString = br.readLine()) != null) {
+            int startOfStandartStr = bufString.indexOf("$GP");
+            if (startOfStandartStr == -1) continue;
+
+            f = getStrWithFormat(standarts, bufString, f, startOfStandartStr);
+        }
+        return bufString != null;
+    }
+
+    private static boolean getStrWithFormat(Standarts standarts, String bufString, boolean f, int startOfStandartStr) throws Exception {
+        String[] arrOfLocationDataInStrings;
+        arrOfLocationDataInStrings = bufString.substring(startOfStandartStr).split(",");
+        switch (arrOfLocationDataInStrings[0]) {
+            case "$GPGGA":
+                ParserCordFormats.GGA_Parser(arrOfLocationDataInStrings, standarts.gga);break;
+            case "$GPGSV":
+                ParserCordFormats.GSV_Parser(arrOfLocationDataInStrings, standarts.gsv);break;
+            case "$GPGSA":
+                ParserCordFormats.GSA_Parser(arrOfLocationDataInStrings, standarts.gsa);break;
+            case "$GPRMC":
+                ParserCordFormats.RMC_Parser(arrOfLocationDataInStrings, standarts.rmc);
+                f = false;
+                break;
+            default: throw new Exception("Ошибка считывания файла");
+        }
+        return f;
     }
 }
