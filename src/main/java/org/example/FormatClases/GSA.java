89def20527345b4c4e6cf5e64266a6ffd002c08d
Александр Бахмутов <alexdmitralex@gmail.com>
2022-10-19 15:49:52 +0300

    добавил класск для хранения формата  GSA

---
+++ src/main/java/org/example/FormatClases/GSA.java
@@ -0,0 +1,13 @@
+package org.example.FormatClases;
+
+import java.util.List;
+
+public class GSA {
+    Character Mode1;
+    Integer Mode2;
+    List<Integer> SatelliteUsed;
+    Double PDOP;
+    Double HDOP;
+    Double VDOP;
+    Integer checksum;
+}
