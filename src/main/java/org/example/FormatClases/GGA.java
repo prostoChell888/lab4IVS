37665f2976454ee7a37c9adbc524340576a6621c
Александр Бахмутов <alexdmitralex@gmail.com>
2022-10-19 15:49:36 +0300

    добавил класск для хранения формата  GGA

---
+++ src/main/java/org/example/FormatClases/GGA.java
@@ -0,0 +1,21 @@
+package org.example.FormatClases;
+
+public class GGA {
+
+    Double TimeUTC;//распарсить в часы
+    Double latitude; //распрсить
+    Character indicatorNS;
+    Double longitude;
+    Character indicatorEW;
+    Integer positionFixIndicator;
+    Integer satellitesUsed;
+    Double HDOР;
+    Double MSLAltitude;
+    Character units1;
+    Double geoidSeparation1;
+    Character units2;
+    Integer ageOfDiffCorr;
+    Integer diffrefstationID;
+    Integer checksum;
+
+}
