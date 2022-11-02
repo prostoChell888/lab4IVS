441173e4d7777b4923bcac36e3d6a030894dd9c3
Александр Бахмутов <alexdmitralex@gmail.com>
2022-11-02 16:17:57 +0300

    Добавил метод для отрисовки таблицы без передачи в нее данных

--- src/main/java/org/example/ButtonFactory.java
+++ src/main/java/org/example/ButtonFactory.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.notUse.Location;
 import org.example.newClasses.DBCConnector;
 import org.example.newClasses.FilesHolder;
 
@@ -7,6 +8,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.util.List;
+import java.util.Objects;
 
 import static java.awt.Component.LEFT_ALIGNMENT;
 
@@ -37,9 +39,9 @@ public class ButtonFactory {
 
             try {
                 DBCConnector dbConector = new DBCConnector(DB_URL, DB_USERNAME, DB_PASSWORD);
-                dbConector.addInfoFromRafFile(holderFirstFile.file);
+                //dbConector.addInfoFromRafFile(holderFirstFile.file); todo раскоментировать
                 System.out.println("все ок");
-                FramePrinter.printGeneralWindow(frame, dbConector);
+                FramePrinter.printNewTableWindow(frame, dbConector, new JTable());
             } catch (Exception e) {
                 try {
                     if (holderFirstFile.file == null) {
@@ -92,76 +94,144 @@ public class ButtonFactory {
     }
 
 
-    public static JComboBox createComboBoxChooseAxis
-            (List<Double> cords, List<Location> locationList, StringBuilder nameOfAxis) {
-        Font font = new Font("Verdana", Font.PLAIN, 18);
+    public static JComboBox createFormatChoserButton(JFrame frame, DBCConnector connection) {
+        var choose = new String[]{"GGA", "RMC", "GSA", "GSV"};
+        JComboBox<String> comboBox = new JComboBox<>(choose);
 
-        String[] items = {
-                "Направление",
-                "Время",
-                "Широта",
-                "Долгота",
-                "Высота",
-                "скорость"
-        };
-
-        ActionListener actionListener = e -> {
-            JComboBox box = (JComboBox) e.getSource();
+        comboBox.addActionListener(al -> {
+            JComboBox box = (JComboBox) al.getSource();
             String item = (String) box.getSelectedItem();
-            nameOfAxis.setLength(0);
-            nameOfAxis.append(item);
-            switch (item) {
-                case "Время":
-
-                    for (var cord : locationList)
-                        cords.add(cord.getTimeInHours());
-                    break;
-                case "Широта":
-                    for (var cord : locationList)
-                        cords.add(cord.getLatitudeInDegrees());
-                    break;
-                case "Долгота":
-                    for (var cord : locationList)
-                        cords.add(cord.getLongitudeInDegrees());
-                    break;
-                case "Высота":
-                    for (var cord : locationList)
-                        cords.add(cord.getAltitudeInDegrees());
-                    break;
-                case "Направление":
-                    for (var cord : locationList)
-                        cords.add(cord.getCourse());
-                    break;
-                case "скорость":
-                    for (var cord : locationList)
-                        cords.add(cord.getSpeedInKilPerHour());
-                    break;
+            JTable jTable = new JTable();
+            switch (Objects.requireNonNull(item)) {
+                case "GGA": jTable = getGGA_table();break;
+                case "RMC": jTable = getRMC_table();break;
+                case "GSA": jTable = getGSA_table(); break;
+                case "GSV": jTable = getGSV_table(); break;
             }
-        };
-
-        JComboBox comboBox = new JComboBox(items);
-        comboBox.setFont(font);
-        comboBox.setAlignmentX(LEFT_ALIGNMENT);
-        comboBox.addActionListener(actionListener);
-
+            try {
+                FramePrinter.printNewTableWindow(frame, connection,  jTable);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
         return comboBox;
     }
 
-    public static JButton createGraphButton
-            (JFrame frame, List<Double> listXAxis,
-             List<Double> listYAxis, StringBuilder nameOfXAxis,
-             StringBuilder nameOfYAxis, List<Location> listWithCoordinates) {
-
-        JButton jButton = new JButton("Построить график");
-
-        ActionListener actionListener = e -> {
-            FramePrinter.printGraphWindow(frame, listXAxis,
-                    listYAxis, nameOfXAxis, nameOfYAxis, listWithCoordinates);
-
+    private static JTable getGGA_table() {
+        String[] header = {"Время ч.", "Широта гр.", "N/S Indicator", "Долгота", "E/W Indicator", "фиксация",
+                "Используемы\n спутники", "Горизонтальное\n разбавление\n точности", "Units", "Geoid\n Separation1"
         };
-        jButton.addActionListener(actionListener);
+        List<List<String>> data = DBCConnector.getGGA_inf();
 
-
-        return jButton;
+        return getJTable(data, header);
     }
-}
+
+    private static JTable getRMC_table() {
+        String[] header = {"Время ч.", "Date", "Широта гр.", "N/S Indicator", "Долгота", "E/W Indicator",
+                "Speed Over Ground", "Course Over Ground"
+        };
+        List<List<String>> data = DBCConnector.getRMC_inf();
+
+        return getJTable(data, header);
+    }
+
+    private static JTable getGSA_table() {
+        String[] header = {"Время ч.", "PDOP", "HDOP", "VDOP", "Режим 1", "Режим 2"};
+        List<List<String>> data = DBCConnector.getGSA_inf();
+
+        return getJTable(data, header);
+    }
+
+
+    private static JTable getGSV_table() {
+        String[] header = {"Время ч.", "ID спутник", "азмут", "радиус", "Сила сигнала"};
+        List<List<String>> data = DBCConnector.getGSV_inf();
+        return getJTable(data, header);
+    }
+
+
+
+    private static JTable getJTable(List<List<String>> data, String[] handle) {
+        var dataForTable = new String[data.size()][data.get(0).size()];
+
+        for (int i = 0; i < data.size(); i++){
+            for (int j = 0; j < data.get(0).size(); j++){
+                dataForTable[i][j] = data.get(i).get(j);
+            }
+        }
+        return new JTable(dataForTable, handle);
+    }
+
+        public static JComboBox<String> createComboBoxChooseAxis
+        (List < Double > cords, List < Location > locationList, StringBuilder nameOfAxis){
+            Font font = new Font("Verdana", Font.PLAIN, 18);
+
+            String[] items = {
+                    "Направление",
+                    "Время",
+                    "Широта",
+                    "Долгота",
+                    "Высота",
+                    "скорость"
+            };
+
+            ActionListener actionListener = e -> {
+                JComboBox box = (JComboBox) e.getSource();
+                String item = (String) box.getSelectedItem();
+                nameOfAxis.setLength(0);
+                nameOfAxis.append(item);
+                switch (item) {
+                    case "Время":
+
+                        for (var cord : locationList)
+                            cords.add(cord.getTimeInHours());
+                        break;
+                    case "Широта":
+                        for (var cord : locationList)
+                            cords.add(cord.getLatitudeInDegrees());
+                        break;
+                    case "Долгота":
+                        for (var cord : locationList)
+                            cords.add(cord.getLongitudeInDegrees());
+                        break;
+                    case "Высота":
+                        for (var cord : locationList)
+                            cords.add(cord.getAltitudeInDegrees());
+                        break;
+                    case "Направление":
+                        for (var cord : locationList)
+                            cords.add(cord.getCourse());
+                        break;
+                    case "скорость":
+                        for (var cord : locationList)
+                            cords.add(cord.getSpeedInKilPerHour());
+                        break;
+                }
+            };
+
+            JComboBox<String> comboBox = new JComboBox<>(items);
+            comboBox.setFont(font);
+            comboBox.setAlignmentX(LEFT_ALIGNMENT);
+            comboBox.addActionListener(actionListener);
+
+            return comboBox;
+        }
+
+        public static JButton createGraphButton
+        (JFrame frame, List < Double > listXAxis,
+                List < Double > listYAxis, StringBuilder nameOfXAxis,
+                StringBuilder nameOfYAxis, List < Location > listWithCoordinates){
+
+            JButton jButton = new JButton("Построить график");
+
+            ActionListener actionListener = e -> {
+                FramePrinter.printGraphWindow(frame, listXAxis,
+                        listYAxis, nameOfXAxis, nameOfYAxis, listWithCoordinates);
+
+            };
+            jButton.addActionListener(actionListener);
+
+
+            return jButton;
+        }
+    }
